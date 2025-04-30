7b21436cf0a2d6a7d9289e4a95706f304a828deb
Arian2i <aryanazarnioshe@gmail.com>
2025-04-30 10:52:44 +0330

    add data json file handler

---
+++ data_handler.py
@@ -0,0 +1,14 @@
+import json
+
+def load_json(filename):
+    try:
+        with open(filename, 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
+    except json.JSONDecodeError:
+        return []
+
+def save_json(filename, data):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
